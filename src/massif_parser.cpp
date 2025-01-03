4d85ca9f0e3a5a83165ea4b6598a239993604f8a
Vukasin Markovic <skolskiacc@gmail.com>
2025-01-03 19:25:21 +0100

    Add memory stabilization check after heap memory spikes and fix bugs in detectMemoryLeaks
    Relates to #3.

--- src/massif_parser.cpp
+++ src/massif_parser.cpp
@@ -62,27 +62,68 @@ std::pair<std::map<std::string, std::string>, std::vector<Snapshot>>parseMassifF
 }
 
 
+bool isMemoryStabilized(const std::vector<Snapshot>& snapshots, size_t currentIndex, size_t windowSize = 3){
+    const auto& currentSnap = snapshots[currentIndex];
+    for(size_t i = 1; i<= windowSize && currentIndex + i <= snapshots.size(); i++){
+        if( currentSnap.mem_heap_B < snapshots[currentIndex + i].mem_heap_B){
+            return false;
+        }
+    }
+    return true;
+}
+
 void detectMemoryLeaks(const std::vector<Snapshot>& snapshots) {
     const double MEMORY_JUMP_TRESHOLD = 0.5; // 50% - prag za nagli skok
     const double LARGE_MEMORY_TRESHOLD = 1000000000; // 1GB - prag za veliku memoriju
 
     Snapshot previousSnapshot;
+    bool hasPreviousSnapshot = false;
 
     for(size_t i = 0; i < snapshots.size(); i++){
 
         const auto& snap = snapshots[i];
 
-        if(previousSnapshot.snapshot>=0){
-            double heapJump = std::abs(static_cast<double> (snap.mem_heap_B - previousSnapshot.mem_heap_B)/previousSnapshot.mem_heap_B);
-            double stackJump = std::abs(static_cast<double> (snap.mem_stacks_B - previousSnapshot.mem_stacks_B )/previousSnapshot.mem_stacks_B);
+        if(hasPreviousSnapshot){
+            double heapJump = 0.0;
 
-            if(heapJump > MEMORY_JUMP_TRESHOLD){
+            if(previousSnapshot.mem_heap_B != 0){
+                double heapDiff = static_cast<double>(snap.mem_heap_B - previousSnapshot.mem_heap_B);
+                if(heapDiff>0){
+                    heapJump = heapDiff/previousSnapshot.mem_heap_B;
+                }
+            }
+            else if (snap.mem_heap_B > LARGE_MEMORY_TRESHOLD) {
+                heapJump = 1.0; //TO DO     sa 100% oznacavam znacajan skok sa 0
+            }
+
+            if(heapJump >= MEMORY_JUMP_TRESHOLD){
                 std::cout << "Info: Heap memory jump between snapshot " 
                     << previousSnapshot.snapshot
                     << " and snapshot "
                     << snap.snapshot
                     << " is " << heapJump*100 << "%\n" ;
+
+                if(isMemoryStabilized(snapshots, i)){
+                    std::cout << "Note: Memory stabilized after snapshot " << snap.snapshot << ".\n";
+                }
+                else{
+                    std::cout << "Warning: Memory continues to grow after snapshot " << snap.snapshot << ".\n";
+                }
             }
+
+
+            double stackJump = 0.0;
+
+            if (previousSnapshot.mem_stacks_B != 0){
+                double stackDiff = static_cast<double>(snap.mem_stacks_B - previousSnapshot.mem_stacks_B);
+                if(stackDiff>0){
+                    stackJump = stackDiff/previousSnapshot.mem_stacks_B;
+                }
+            }
+            else if (snap.mem_stacks_B > LARGE_MEMORY_TRESHOLD) {
+                stackJump = 1.0; //TO DO     sa 100% oznacavam znacajan skok sa 0
+            }
+            
 
             if(stackJump > MEMORY_JUMP_TRESHOLD){
                 std::cout << "Info: Stack memory jump between snapshot " 
@@ -91,6 +132,7 @@ void detectMemoryLeaks(const std::vector<Snapshot>& snapshots) {
                     << snap.snapshot
                     << " is " << stackJump*100 << "%\n";
             }
+            
         }
         
         const long BYTES_TO_MB = 1024 * 1024;
@@ -105,7 +147,12 @@ void detectMemoryLeaks(const std::vector<Snapshot>& snapshots) {
                 << ": " << snap.mem_stacks_B / BYTES_TO_MB << "MB\n";
         }
 
-        previousSnapshot = snap;        
+        // Azuriramo prethodni snapshot samo ako je trenutni snapshot validan - posto masif ima i laznih
+        if (snap.mem_heap_B != 0 || snap.mem_stacks_B != 0 || snap.mem_heap_extra_B != 0) {
+            previousSnapshot = snap;
+            hasPreviousSnapshot = true;
+        }
+
     }
 
 }
