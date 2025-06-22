573136e7337e2a8b1687471fca55cc01f04644cb
Stefan <drljevic.stefan00@gmail.com>
2025-06-22 22:12:30 +0200

    refactor: extract file selection logic into FileSelector class
    - Created FileSelector class to handle all file choosing logic
    - Delegated QFileDialog interaction based on selected mode (source, binary, or output)
    - MainWindow now uses FileSelector for cleaner UI logic
    - Simplifies responsibilities and prepares for further modularization
    
    Relates to #12

---
+++ QTCreator/2024_Research_MassifCheck/fileselector.h
@@ -0,0 +1,40 @@
+#ifndef FILESELECTOR_H
+#define FILESELECTOR_H
+
+#include <QObject>
+#include <QFileDialog>
+#include <QProcess>
+#include <QDir>
+#include <QCoreApplication>
+#include <QMessageBox>
+#include <QRegularExpression>
+#include "ModeEnum.h"
+
+class FileSelector : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileSelector(QObject *parent = nullptr);
+
+    inline QString getFileName() const { return this->fileName; };
+    inline QString getFilePath() const { return this->filePath; };
+    inline QString getOutFileName() const {return this->outFileName;};
+    inline QString getOutFilePath() const {return this->outFilePath;};
+
+
+    void selectFile(QWidget* parent, Mode mode);
+    QString convertWindowsPathToWsl(const QString& winPath);
+    QString getDirectoryPath(QString filePath);
+    QString replaceCppWithOut(const QString fileName);
+    void clearFileSelection();
+
+signals:
+
+private:
+    QString fileName;
+    QString filePath;
+    QString outFileName;
+    QString outFilePath;
+};
+
+#endif // FILESELECTOR_H
