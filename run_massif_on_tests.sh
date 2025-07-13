c9a5b421c29a8ae6d237b682a32191662e96a798
Vukasin Markovic <skolskiacc@gmail.com>
2025-07-13 18:16:37 +0200

    feat: Update test script to support compilation with both gcc and g++ compilers
    Relates to #21.

--- run_massif_on_tests.sh
+++ run_massif_on_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST_DIR="test"
-TEST_FILES=("test1.c" "test2.c")
+TEST_FILES=("test1.c" "test2.c" "test3.cpp")
 OUTPUT_DIR="massif_files"
 OUTPUT_FILE_BASE="massif.out"
 
@@ -10,13 +10,27 @@ mkdir -p $OUTPUT_DIR
 for i in "${!TEST_FILES[@]}"; do
   TEST_FILE="${TEST_DIR}/${TEST_FILES[$i]}"
   OUTPUT_FILE="${OUTPUT_DIR}/${OUTPUT_FILE_BASE}.$((i+1))"
+  EXE_FILE="${TEST_DIR}/$(basename "${TEST_FILE%.*}")"
 
-  EXE_FILE="${TEST_DIR}/$(basename ${TEST_FILE%.c})"
+
+  # compiler based on extension
+  EXT="${TEST_FILE##*.}"
+  if [ "$EXT" == "c" ]; then
+    COMPILER="gcc"
+  elif [ "$EXT" == "cpp" ]; then
+    COMPILER="g++"
+  else
+    echo "Unknown extension: $TEST_FILE"
+    continue
+  fi
+
+
+  # compiling if exe don't exist
   if [ ! -f "$EXE_FILE" ]; then
-    echo "Compiling $TEST_FILE..."
-    gcc -o "$EXE_FILE" "$TEST_FILE"
+    echo "Compiling $TEST_FILE with $COMPILER..."
+    $COMPILER -o "$EXE_FILE" "$TEST_FILE"
   fi
 
   echo "Running valgrind on $EXE_FILE..."
-  valgrind --tool=massif --massif-out-file="$OUTPUT_FILE" ./$EXE_FILE
+  valgrind --tool=massif --massif-out-file="$OUTPUT_FILE" ./$EXE_FILE  > /dev/null 2>&1
 done
